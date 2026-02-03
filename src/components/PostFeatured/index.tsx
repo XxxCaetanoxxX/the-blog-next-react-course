20800b2aba81dc8830c157b1628edb2cb0541242
caetano.cesar <caetano.cesar@defensoria.mg.def.br>
2026-02-03 10:15:30 -0300

    fea: implementando repositório com cache

--- src/components/PostFeatured/index.tsx
+++ src/components/PostFeatured/index.tsx
@@ -1,21 +1,22 @@
 import { PostCoverImage } from "../PostCoverImage";
-import { PostHeading } from "../PostHeading";
 import { PostSummary } from "../PostSummary";
+import { findAllPublicPosts } from "@/lib/post/queries";
 
-export function PostFeatured() {
-    const slug = 'qualquer'
-    const postLink = `/post/${slug}`
+export async function PostFeatured() {
+    const posts = await findAllPublicPosts();;
+    const post = posts[0];
+
+    const postLink = `/post/${post.slug}`
     return (
         <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
 
-            <PostCoverImage imageSrc="/images/bryen_5.png" imageAlt="Bryen 5" url={postLink} />
-
+            <PostCoverImage imageSrc={post.coverImageUrl} imageAlt={post.title} url={postLink} />
             <PostSummary
                 postLink={postLink}
                 postHeading="h1"
-                createdAt={'2024-06-01T10:00:00Z'}
-                title={'Post Destaque: Explorando o Bryen 5'}
-                excerpt={'Descubra as funcionalidades e inovações do Bryen 5, o mais recente lançamento em tecnologia de ponta.'}
+                createdAt={post.createdAt}
+                title={post.title}
+                excerpt={post.excerpt}
             />
         </section>
     )
