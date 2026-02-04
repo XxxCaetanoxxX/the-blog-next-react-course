c1e7ccf45fb37b573f58a2c2dcc25bdbb3b46e7e
Caetano César <caetanocesar35@gmail.com>
2026-02-04 14:53:03 -0300

    not found page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,35 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Container } from "@/components/Container";
+import { Header } from "@/components/header";
+import { Footer } from "@/components/Footer";
 
 export const metadata: Metadata = {
-  title: "The blog - blog com nextjs",
-  description: "Essa é a descrição do meu blog feito com Next.js",
+  title: {
+    default: "The Blog - Este é um blog feito com Next.js",
+    template: "%s | The Blog",
+  },
+  description: "Essa é a descrição dessa página",
 };
 
 type RootLayoutProps = {
   children: React.ReactNode;
 };
 
-export default function RootLayout({children}: Readonly<RootLayoutProps>) {
+export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
   return (
-    <html lang="pt-BR" 
+    <html lang="pt-BR"
     // className="dark"
     >
       <body>
-        {children}
+
+        <Container>
+          <Header />
+
+          {children}
+
+          <Footer />
+        </ Container>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,4 @@
-import { Container } from "@/components/Container";
-import { Header } from "@/components/header";
-import { PostCoverImage } from "@/components/PostCoverImage";
 import { PostFeatured } from "@/components/PostFeatured";
-import { PostHeading } from "@/components/PostHeading";
 import { PostsList } from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
 import { Suspense } from "react";
@@ -10,9 +6,7 @@ import { Suspense } from "react";
 export default async function Home() {
 
   return (
-    <Container>
-      <Header />
-
+    <>
       <Suspense fallback={<SpinLoader />}>
         <PostFeatured />
       </Suspense>
@@ -20,10 +14,6 @@ export default async function Home() {
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
       </Suspense>
-
-      <footer>
-        <h1 className="text-6xl font-bold text-center py-8">Aqui é o footer</h1>
-      </footer>
-    </ Container>
+    </>
   );
 }
