49a66c71dcd5ba1f82f604ad1df482347e5c7f41
Caetano César <caetanocesar35@gmail.com>
2026-03-06 18:50:39 -0300

    criando metodos no repo

--- src/actions/post/create-post-action.ts
+++ src/actions/post/create-post-action.ts
@@ -1,6 +1,16 @@
 'use server';
 
-import { PublicPost } from "@/dto/post/dto";
+import { PostCreateSchema } from "@/lib/post/validations";
+import { makePartialPublicPost, PublicPost } from "@/dto/post/dto";
+import { getZodErrorMessages } from "@/utils/get-zod-error-messages";
+import { PostModel } from "@/modelos/post/post-model";
+import { v4 as uuidV4 } from 'uuid';
+import { makeSlugFromText } from "@/utils/make-slug-from-text";
+import { drizzleDb } from "@/db/drizzle";
+import { postsTable } from "@/db/drizzle/schemas";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import { postRepository } from "@/repositories/post";
 
 type CreatePostActionState = {
     formState: PublicPost;
@@ -21,10 +31,42 @@ export async function createPostAction(
     }
 
     const formDataToObject = Object.fromEntries(formData.entries());
-    console.log(formDataToObject);
 
-    return {
-        formState: prevState.formState,
-        errors: [],
+    const zodParsedObject = PostCreateSchema.safeParse(formDataToObject);
+
+    if (!zodParsedObject.success) {
+        const errors = getZodErrorMessages(zodParsedObject.error.format());
+        return {
+            errors,
+            formState: makePartialPublicPost(formDataToObject),
+        }
     }
+
+    const validPostData = zodParsedObject.data;
+    const newPost: PostModel = {
+        ...validPostData,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString(),
+        id: uuidV4(),
+        slug: makeSlugFromText(validPostData.title),
+    }
+
+    try {
+        await postRepository.create(newPost);
+    } catch (e: unknown) {
+        if (e instanceof Error) {
+            return {
+                errors: [e.message],
+                formState: newPost
+            }
+        }
+
+        return{
+            errors: ['Erro Desconhecido.'],
+            formState: newPost
+        }
+    }
+
+    revalidatePath('/posts');
+    redirect(`/admin/post/${newPost.id}`);
 }
--- src/actions/post/delete-post-action.ts
+++ src/actions/post/delete-post-action.ts
@@ -1,34 +1,35 @@
 'use server';
 
-import { drizzleDb } from "@/db/drizzle";
-import { postsTable } from "@/db/drizzle/schemas";
 import { postRepository } from "@/repositories/post";
 import { asyncDelay } from "@/utils/async-delay";
-import { eq } from "drizzle-orm";
 import { revalidatePath } from "next/cache";
 
 export async function deletePostAction(id: string) {
     //TODO: CHECAR LOGIN 
     await asyncDelay(2000);
 
-    if(!id || typeof id !== 'string') {
+    if (!id || typeof id !== 'string') {
         return {
             error: 'Dados inválidos',
         }
     }
 
-    const post = await postRepository.findById(id).catch(() => undefined);
+    let post;
+    try {
+        post = await postRepository.delete(id);
+    } catch (e: unknown) {
+        if (e instanceof Error) {
+            return {
+                error: e.message,
 
-    if (!post) {
+            }
+        }
+
         return {
-            error: 'Post não existe',
+            error: 'Erro Desconhecido',
         }
     }
 
-    //TODO mover para repositorio
-    await drizzleDb.delete(postsTable).where(eq(postsTable.id, id));
-
-    //TODO revalidateTag ou revalidatePath
     revalidatePath('/admin/post');
     revalidatePath('/');
 
